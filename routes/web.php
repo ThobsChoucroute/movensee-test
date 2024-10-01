23848208847ccb5ba627646b18d69898c87bca2f
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-10-01 16:51:27 +0200

    fix bad http redirect code

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect("/", "players", 200);
+Route::redirect("/", "players", 302);
 Route::apiResource('players', PlayerController::class);
